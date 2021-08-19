5d49871e67b1e75cebd99a31acb6b3835e230e89
Sujoy09 <mlflarkyy09@gmail.com>
2021-08-19 17:47:41 +0530

    ADDED: Codes for Cab Invoice Service.

---
+++ src/test/java/CabServiceTest.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CabServiceTest {
+    InvoiceGenerator invoiceGenerator = null;
+
+    @Before
+    public void setUp() throws Exception {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25,fare,0.0);
+    }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinimumFare() {
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] rides = { new Ride(2.0, 5),
+                         new Ride(0.1, 1),
+                       };
+        InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,30.0);
+        Assert.assertEquals(expectedInvoiceSummary, summary);
+    }
+}
